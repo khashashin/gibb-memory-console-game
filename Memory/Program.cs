839f6400acc53d6ad9848b7f11e7f2edf82ca8f2
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2019-11-04 14:29:52 +0100

    first challenge is done

--- Memory/Program.cs
+++ Memory/Program.cs
@@ -10,6 +10,20 @@ namespace Memory
     {
         static void Main(string[] args)
         {
+            ZeigeAnleitung();
+        }
+
+        public static void ZeigeAnleitung()
+        {
+            Console.WriteLine("Hinter den '?' verstecken sich Symbole, die paarweise vorkommen. Finden Sie diese!");
+            Console.WriteLine("Wählen Sie zwei Positionen zum Aufdecken in der Form: Zeile1Spalte1Zeile2Spalte2,");
+            Console.WriteLine("z.B 2142 vergleicht das Symbol in Zeile 2 und Spalte 1 mit dem Symbol in Zeile 4 und Spalte 2");
+            Console.WriteLine("  1  2  3  4");
+            Console.WriteLine("1 ?  ?  ?  ?");
+            Console.WriteLine("2 ☺  ?  ?  ?");
+            Console.WriteLine("3 ?  ?  ?  ?");
+            Console.WriteLine("4 ?  ☺  ?  ?");
+            Console.ReadLine();
         }
     }
 }
